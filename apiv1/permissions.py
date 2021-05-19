dfb4f8ce4df817c43566cb7d9043c0a53fa31510
takasany <jackmacman2@gmail.com>
2021-05-20 02:12:39 +0900

    Add permission class for favorite posts

--- apiv1/permissions.py
+++ apiv1/permissions.py
@@ -2,7 +2,15 @@ from rest_framework.permissions import BasePermission
 
 
 class UsersPostPermission(BasePermission):
+    """取得した投稿がログインユーザーのものかどうか判定するクラス"""
 
-    # 投稿とそれを取得しようとしているユーザーのidが同じかを検証
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user or request.user.is_superuser
+
+
+class MyFavoritePostsPermission(BasePermission):
+    """取得した投稿がログインユーザーのお気に入りかどうか判定するクラス"""
+
+    def has_object_permission(self, request, view, obj):
+        favorite_posts_list = request.user.favorite_posts.all()
+        return obj in favorite_posts_list or request.user.is_superuser
